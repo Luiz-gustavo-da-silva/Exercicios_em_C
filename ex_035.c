a1e15d057985cb47533e8e3c4edcb2b3e542ecea
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2022-06-06 12:06:51 -0300

    Update ex_035.c

--- ex_035.c
+++ ex_035.c
@@ -10,21 +10,27 @@ typedef struct pessoa{
     char sexo;
 } pessoas;
 
-// Não sei o que fazer aqui!
-// void criar(char nome[50], int idade, char sexo){
 
-// }
-void inserir(char nome[50], int idade, char sexo, pessoas *vetor, int tamanho){
-    strcpy(vetor[tamanho].nome, nome);
-    vetor[tamanho].idade=idade;
-    vetor[tamanho].sexo=sexo;
+pessoas criar(char nome[50], int idade, char sexo){
+  pessoas vetor[1];
+  
+  strcpy(vetor[0].nome, nome);
+  vetor[0].idade=idade;
+  vetor[0].sexo=sexo;
+  
+  return vetor[0];
 }
 
-int deletar(char nome[50], int idade, char sexo, pessoas *vetor, int tamanho){
-    int cont=0;
+void inserir(pessoas *vetor, pessoas vetorparcial, int tamanho){
+    vetor[tamanho]=vetorparcial;
+}
+
+int deletar(pessoas *vetor, pessoas vetorparcial, int tamanho){  
+  int cont=0;
+  
     for(int i=0; i<tamanho;i++){
-        if(strcmp(vetor[i].nome,nome)==0 && vetor[i].idade==idade && vetor[i].sexo==sexo){
-            for (int a = i; a < tamanho; a++)
+        if(strcmp(vetor[i].nome, vetorparcial.nome) == 0 && vetor[i].idade == vetorparcial.idade && vetor[i].sexo == vetorparcial.sexo){
+           for (int a = i; a < tamanho; a++)
             {
                 vetor[a]=vetor[a+1];
             }  
@@ -45,13 +51,16 @@ void imprimir(pessoas*vetor, int t){
 
 int main(){
     pessoas*vetor;
+    pessoas vetorparcial[1];
     int tamanhovetor=2;
     int tamanho=0;
+    int criarstruct=1;
     int t=0;
     char nome[50];
     char opcao='a';
     int idade=0;
     char sexo;
+  
     vetor = calloc(1, sizeof(pessoas));
 
     
@@ -66,12 +75,16 @@ int main(){
         scanf(" %c", &sexo);	
 
         if(opcao=='i'){
-            inserir(nome, idade, sexo, vetor, tamanho);
+            vetorparcial[0]=criar(nome, idade, sexo);
+            inserir(vetor, vetorparcial[0], tamanho);
             vetor = realloc(vetor, sizeof(pessoas)*tamanhovetor);
             tamanhovetor++;
             tamanho++;
         }else if(opcao=='d'){
-            int sn = deletar(nome, idade, sexo, vetor, tamanho);
+            vetorparcial[0]=criar(nome, idade, sexo);
+          
+            int sn = deletar(vetor, vetorparcial[0], tamanho);
+          
             if(sn==1){
                 tamanhovetor--;
                 tamanho--;
